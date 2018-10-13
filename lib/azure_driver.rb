ba5405d6713d2316623a2c8a797c10ce7e0bb42f
slntopp <slnt_opp@pm.me>
2018-10-13 11:54:39 +0300

    LICENSE

--- lib/azure_driver.rb
+++ lib/azure_driver.rb
@@ -14,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
 # See the License for the specific language governing permissions and        #
 # limitations under the License.                                             #
-#--------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------- #
 
 ONE_LOCATION = ENV["ONE_LOCATION"] if !defined?(ONE_LOCATION)
 
