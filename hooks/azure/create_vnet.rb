6b46048be432298e277a7c148871d086f8c822d3
Mikita <admin@Admins-MBP.fritz.box>
2019-06-17 18:06:25 +0200

    VNet resource init

---
+++ hooks/azure/create_vnet.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# -------------------------------------------------------------------------- #
+# Copyright 2018-2019, IONe Cloud Project, Support.by                        #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+# -------------------------------------------------------------------------- #
+
+ONE_LOCATION = ENV["ONE_LOCATION"] if !defined?(ONE_LOCATION)
+
+if !ONE_LOCATION
+    RUBY_LIB_LOCATION = "/usr/lib/one/ruby" if !defined?(RUBY_LIB_LOCATION)
+    ETC_LOCATION      = "/etc/one/" if !defined?(ETC_LOCATION)
+else
+    RUBY_LIB_LOCATION = ONE_LOCATION + "/lib/ruby" if !defined?(RUBY_LIB_LOCATION)
+    ETC_LOCATION      = ONE_LOCATION + "/etc/" if !defined?(ETC_LOCATION)
+end
+
+$: << RUBY_LIB_LOCATION
+
+require 'azure_driver'
+
+network_id   = ARGV[0]
+
+template = OpenNebula::VirtualNetwork.new_with_id(network_id, OpenNebula::Client.new)
+rc = template.info
+if OpenNebula.is_error?(rc)
+    STDERR.puts rc.message
+    exit 1
+end
+
